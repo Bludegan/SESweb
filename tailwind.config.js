7177e6cbf77b46fea8dea7b288c22fc4325811e2
Bludegan <angel.flores204305@potros.itson.edu.mx>
2023-04-26 01:07:27 -0700

    Se integro el proyecto con el repositorio

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["Frontend/index.html,Frontend/Login.html,Frontend/Menu.html"],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+  ],
+}
+
